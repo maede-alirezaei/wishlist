2f07558cbe2c3c9e08f6d273c4e470e9e8f5b6c1
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2024-04-23 14:17:44 +0200

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { WishItem } from '../shared/models/wishItem';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items = [
+    new WishItem('Learn Angular'),
+    new WishItem('Find an apartment',true),
+    new WishItem('Call mom'),
+  ];
   title = 'wishlist';
 }
